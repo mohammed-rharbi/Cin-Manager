c893d83de56eaed667b19b153b0662d541c299bb
mohammed-rharbi <rharbi383@gmail.com>
2024-10-23 11:47:43 +0100

    fix : fix space

--- src/models/seatSchema.js
+++ src/models/seatSchema.js
@@ -1,12 +1,9 @@
 const mongoose = require('mongoose');
 
 const seatSchema = new mongoose.Schema({
-
-
     seateNumber : {type:Number , required:true},
     isAvailable : {type:Boolean , default:true},
     room:{type:mongoose.Schema.Types.ObjectId , ref:'Room' , required:true}
-
 } , {timestamps:true});
 
 module.exports = mongoose.model('Seat' , seatSchema);   
